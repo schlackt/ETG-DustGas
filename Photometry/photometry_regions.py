a4f3e9a215f1cb2eb8330c9f6ef669c0bb55733e
schlackt <40439910+schlackt@users.noreply.github.com>
2018-07-20 11:11:53 -0700

    added region subtraction and fixed angle bug

--- Photometry/photometry_regions.py
+++ Photometry/photometry_regions.py
@@ -46,22 +46,31 @@ def translate(line, coord_info, scale):
         ra = positionStringtoInt(remainder_commasplit[0], True)
         #the second entry of remainder_commasplit is declination. Pass directly to the converter.
         dec = positionStringtoInt(remainder_commasplit[1], False)
-        #the third entry is the radius for circular apertures, or the semiminor axis for elliptical apertures
-        semiminor = float(remainder_commasplit[2].split('"')[0]) #cut the quotation mark away from the number
+        #the third entry is the radius for circular apertures, or the one of the axes for elliptical apertures
+        radius1 = float(remainder_commasplit[2].split('"')[0]) #cut the quotation mark away from the number
 
-        #convert ra, dec, and semiminor to pixels
+        #convert ra, dec, and radius to pixels
         x, y = coord_info.all_world2pix(ra, dec, 0)
-        semiminor_pix = arcsecToPix(semiminor, scale)
+        radius1_pix = arcsecToPix(radius1, scale)
 
         if line_array[0] == 'circle': #if we have a circular object, we're all done!
-            return (galaxy_aperture, CircularAperture((x, y), semiminor_pix))
+            return (galaxy_aperture, CircularAperture((x, y), radius1_pix))
         else: #otherwise, we still need to grab some information
-            #the fourth entry is the semimajor axis for elliptical apertures
-            semimajor = float(remainder_commasplit[3].split('"')[0]) #cut the quotation mark away from the number
-            semimajor_pix = arcsecToPix(semimajor, scale)
+            #the fourth entry is the other axis for elliptical apertures
+            radius2 = float(remainder_commasplit[3].split('"')[0]) #cut the quotation mark away from the number
+            radius2_pix = arcsecToPix(radius2, scale)
             #the fifth entry is the rotation angle for elliptical apertures
-            angle = float(remainder_commasplit[4].split(')')[0]) * math.pi / 180 + math.pi / 2 #cut the closing parenthesis away from the number and convert to radians
-            return (galaxy_aperture, EllipticalAperture((x, y), semimajor_pix, semiminor_pix, angle))
+            angle = float(remainder_commasplit[4].split(')')[0]) * math.pi / 180 #cut the closing parenthesis away from the number and convert to radians
+            #determine which axis is which
+            #the angle reported by ds9 is the angle of radius2 east of north. it needs to be converted to angle of th semimajor axis above the positive x axis
+            if radius1 < radius2:
+                semiminor = radius1_pix
+                semimajor = radius2_pix
+                angle = angle + math.pi() / 2
+            else:
+                semiminor = radius2_pix
+                semimajor = radius1_pix
+            return (galaxy_aperture, EllipticalAperture((x, y), semimajor, semiminor, angle))
     else:
         return None
 
@@ -196,32 +205,58 @@ print('\n')
 
 lines = regions.readlines()
 background_apertures = []
-galaxy_aperture = None
+galaxy_apertures = []
 for line in lines: #make the apertures from the regions file and sort into a list of background apertures and a galaxy aperture
     translation = translate(line, values_wcs, pix_scale)
     if translation != None:
         if translation[0]:
-            galaxy_aperture = translation[1]
+            galaxy_apertures.append(translation[1])
         else:
             background_apertures.append(translation[1])
 
-galaxy_photometry = aperture_photometry(value_data, galaxy_aperture, error_data).as_array()[0] #get photometry results of the galaxy as an array
-if len(galaxy_photometry) == 4:
-    galaxy_flux = (galaxy_photometry[3], 0) #if no error was calculated, return 0 for error
-else:
-    galaxy_flux = (galaxy_photometry[3], galaxy_photometry[4])
+#sort the list of galaxy apertures from highest area to lowest area
+aperture_areas = []
+for aperture in galaxy_apertures:
+    aperture_areas.append((aperture.area(), galaxy_apertures.index(aperture)))
+aperture_areas_sorted = sorted(aperture_areas, key=lambda aperture: aperture[0]) #sort by area
+galaxy_apertures_sorted = []
+for aperture in aperture_areas_sorted:
+    galaxy_apertures_sorted.insert(0, galaxy_apertures[aperture[1]])
 
+#get background
 if len(background_apertures) == 0:
     sky = (0, 0)
 else:
     sky = background(value_data, error_data, background_apertures)
 
-final_flux = (galaxy_flux[0] - sky[0] * galaxy_aperture.area()) * multiplier #scale background and subtract
-error_sky = (math.sqrt(galaxy_flux[1] * galaxy_flux[1] + sky[1] * galaxy_aperture.area() * sky[1] * galaxy_aperture.area())) * multiplier #calculate error from sky
-if spire == 'y': #calculate calibration error
-    error_cal = final_flux * 0.07
-else:
-    error_cal = final_flux * 0.05
-final_error = math.sqrt(error_cal * error_cal + error_sky * error_sky)
+#make list of photometry results for each aperture in the form (flux, sky error, total error)
+results = []
+for galaxy_aperture in galaxy_apertures_sorted:
+    galaxy_photometry = aperture_photometry(value_data, galaxy_aperture, error_data).as_array()[0] #get photometry results of the galaxy as an array
+    if len(galaxy_photometry) == 4:
+        galaxy_flux = (galaxy_photometry[3], 0) #if no error was calculated, return 0 for error
+    else:
+        galaxy_flux = (galaxy_photometry[3], galaxy_photometry[4])
 
-print('Flux: ' + str(final_flux) + ' Jy\nSky Error: ' + str(error_sky) + ' Jy\nTotal Error: ' + str(final_error) + ' Jy')
+    final_flux = (galaxy_flux[0] - sky[0] * galaxy_aperture.area()) * multiplier #scale background and subtract
+    error_sky = (math.sqrt(galaxy_flux[1] * galaxy_flux[1] + sky[1] * galaxy_aperture.area() * sky[1] * galaxy_aperture.area())) * multiplier #calculate error from sky
+    if spire == 'y': #calculate calibration error
+        error_cal = 0.07
+    else:
+        error_cal = 0.05
+    final_error = math.sqrt(final_flux * error_cal * final_flux * error_cal + error_sky * error_sky)
+    results.append((final_flux, error_sky, final_error))
+
+#now do region subtraction and print results
+i = 0
+while i < len(results):
+    if i == 0:
+        print('Galaxy:\n\tFlux: ' + str(results[i][0]) + ' Jy\n\tSky Error: ' + str(results[i][1]) + ' Jy\n\tTotal Error: ' + str(results[i][2]) + ' Jy\n')
+    else:
+        region_flux = results[i - 1][0] - results[i][0]
+        region_sky_error = math.sqrt(results[i - 1][1] * results[i - 1][1] + results[i][1] * results[i][1])
+        region_total_error = math.sqrt(region_sky_error * region_sky_error + region_flux * error_cal * region_flux * error_cal)
+        print('Next Region:\n\tFlux: ' + str(region_flux) + ' Jy\n\tSky Error: ' + str(region_sky_error) + ' Jy\n\tTotal Error: ' + str(region_total_error) + ' Jy\n')
+    if i == len(results) - 1:
+        print('Center:\n\tFlux: ' + str(results[i][0]) + ' Jy\n\tSky Error: ' + str(results[i][1]) + ' Jy\n\tTotal Error: ' + str(results[i][2]) + ' Jy\n')
+    i += 1
